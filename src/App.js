ac38bbad7b6243ce86171819439885fc1fe85ee7
Manuel Zropf <zropfmanuel@gmail.com>
2023-04-27 21:30:32 +0200

    get-data-and-search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./phone-data.json";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [phoneData, setPhoneData] = useState(data);
+
+  const handleSearch = (e) => {
+    const filteredData = data.filter(
+      (phone) =>
+        phone.name.toLowerCase().includes(e.target.value.toLowerCase()) ||
+        phone.phone.includes(e.target.value)
+    );
+    setPhoneData(filteredData);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Phonebook</h1>
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => handleSearch(e)}
+      />
+      <h2>Contacts</h2>
+
+      <div className="contact-list">
+        {phoneData.map((phone) => (
+          <div className="contact-card">
+            <p>{phone.name}</p>
+            <a href="">{phone.phone}</a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
